2d10dc84ffafe31d7992ab0ade822b39adfa123f
simonriley999 <892949563@qq.com>
2020-06-16 19:44:14 +0800

    checkin已完成

--- src/main/java/com/hotel/pojo/checkin.java
+++ src/main/java/com/hotel/pojo/checkin.java
@@ -1,25 +1,27 @@
 package com.hotel.pojo;
 
+import java.math.BigDecimal;
 import java.text.DecimalFormat;
+import java.util.Date;
 
 public class checkin {
     private Integer id;
     private String no;
     private String room_number;
     private Integer room_type_id;
-    private DecimalFormat standard_price;
-    private DecimalFormat discount_price;
-    private DecimalFormat deposit;
+    private BigDecimal standard_price;
+    private BigDecimal discount_price;
+    private BigDecimal deposit;
     private String customer_name;
     private Integer card_type;
     private String card_number;
     private String phone;
-
-
+    private Date arrive_time;
+    private Date leave_time;
     private Integer customer_size;
     private Integer user_id;
     private Integer customer_id;
-    private DecimalFormat customer_price;
+    private BigDecimal customer_price;
     private Integer breakfast_or_not;
     private Integer remind_or_not;
     private String note;
