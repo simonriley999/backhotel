920b12a68290222c2b747bf3d74838a3db0a6bf3
simonriley999 <892949563@qq.com>
2020-06-16 17:41:30 +0800

    仅含未完成的checkin

---
+++ src/main/java/com/hotel/pojo/checkin.java
@@ -0,0 +1,26 @@
+package com.hotel.pojo;
+
+import java.text.DecimalFormat;
+
+public class checkin {
+    private Integer id;
+    private String no;
+    private String room_number;
+    private Integer room_type_id;
+    private DecimalFormat standard_price;
+    private DecimalFormat discount_price;
+    private DecimalFormat deposit;
+    private String customer_name;
+    private Integer card_type;
+    private String card_number;
+    private String phone;
+
+
+    private Integer customer_size;
+    private Integer user_id;
+    private Integer customer_id;
+    private DecimalFormat customer_price;
+    private Integer breakfast_or_not;
+    private Integer remind_or_not;
+    private String note;
+}
---
+++ src/test/java/test/MyTest.java
@@ -0,0 +1,10 @@
+package test;
+
+import com.hotel.pojo.RoomType;
+
+public class MyTest {
+    public static void main(String[] args) {
+        RoomType roomType = new RoomType();
+        System.out.println(roomType.getId());
+    }
+}
