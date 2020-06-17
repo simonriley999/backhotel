491f02ddc46fab2a2bca45ee81bec7b3dc976880
jiazongyang <1871159638@qq.com>
2020-06-17 08:56:21 +0800

    加注释

--- src/main/java/com/hotel/pojo/Room.java
+++ src/main/java/com/hotel/pojo/Room.java
@@ -5,11 +5,18 @@ import lombok.Data;
 @Data
 public class Room {
     private Integer id;
+    //房号
     private String room_number;
+    //状态 1入住 2空闲 3预定
     private Integer state;
+    //标准价
     private double standard_price;
+    //折后价
     private double discount_price;
+    //普通会员价
     private double ncustomer_price;
+    //VIP会员价
     private double vcustomer_price;
+    //备注
     private String note;
 }
--- src/main/java/com/hotel/pojo/RoomReserve.java
+++ src/main/java/com/hotel/pojo/RoomReserve.java
@@ -4,20 +4,35 @@ import java.util.Date;
 
 public class RoomReserve {
     private Integer id;
+    //预定单号
     private String no;
+    //房间号
     private String room_number;
+    //客房类型
     private Integer room_type_id;
+    //标准件
     private double standard_price;
+    //折后价
     private double discount_price;
+    //押金
     private double deposit;
+    //预订人
     private String customer_name;
+    //证件类型  1身份证2护照3军官证
     private Integer card_type;
+    //证件号
     private String card_number;
+    //联系电话
     private String phone;
+    //到店时间
     private Date arrive_time;
+    //离店时间
     private Date leave_time;
+    //入住人数
     private  Integer customer_size;
+    //操作员id
     private  Integer user_id;
+    //会员编号
     private  Integer customer_id;
 
 }
