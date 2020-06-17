f05390cdcca8a5e8b38e0bae27c821b870d75f22
simonriley999 <892949563@qq.com>
2020-06-17 08:59:28 +0800

    为数据库添加了注释

--- src/main/java/com/hotel/pojo/RoomType.java
+++ src/main/java/com/hotel/pojo/RoomType.java
@@ -4,9 +4,9 @@ import lombok.Data;
 
 @Data
 public class RoomType {
-    private Integer	id;
-    private String name;
-    private Integer	size;
-    private String note;
+    private Integer	id;//自增id
+    private String name;//客房名称
+    private Integer	size;//额定人数
+    private String note;//备注
 
 }
--- src/main/java/com/hotel/pojo/checkin.java
+++ src/main/java/com/hotel/pojo/checkin.java
@@ -5,24 +5,24 @@ import java.text.DecimalFormat;
 import java.util.Date;
 
 public class checkin {
-    private Integer id;
-    private String no;
-    private String room_number;
-    private Integer room_type_id;
-    private BigDecimal standard_price;
-    private BigDecimal discount_price;
-    private BigDecimal deposit;
-    private String customer_name;
-    private Integer card_type;
-    private String card_number;
-    private String phone;
-    private Date arrive_time;
-    private Date leave_time;
-    private Integer customer_size;
-    private Integer user_id;
-    private Integer customer_id;
-    private BigDecimal customer_price;
-    private Integer breakfast_or_not;
-    private Integer remind_or_not;
-    private String note;
+    private Integer id;//自增id
+    private String no;//入住单号
+    private String room_number;//房号
+    private Integer room_type_id;//客房类型
+    private BigDecimal standard_price;//标准价
+    private BigDecimal discount_price;//折后价
+    private BigDecimal deposit;//押金
+    private String customer_name;//入住人
+    private Integer card_type;//1身份证2护照3军官证
+    private String card_number;//证件号
+    private String phone;//联系电话
+    private Date arrive_time;//到店时间
+    private Date leave_time;//离店时间
+    private Integer customer_size;//入住人数
+    private Integer user_id;//操作员id
+    private Integer customer_id;//会员编号
+    private BigDecimal customer_price;//VIP会员价
+    private Integer breakfast_or_not;//1提供2不提供
+    private Integer remind_or_not;//1提供2不提供
+    private String note;//备注
 }
